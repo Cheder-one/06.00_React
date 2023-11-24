f8a6d3289e221d845dc2afb835e0b3528f259bca
Cheder-one <cherbadenis1@gmail.com>
2023-11-24 16:57:12 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import TaskInputForm from "./app/components/task-input/taskInputForm";
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <TaskInputForm />
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ src/app/components/task-input/taskInputForm.jsx
@@ -0,0 +1,27 @@
+import { Component } from "react";
+
+class TaskInputForm extends Component {
+  // state = {  }
+
+  render() {
+    return (
+      <footer class="footer">
+        <span class="todo-count">1 items left</span>
+        <ul class="filters">
+          <li>
+            <button class="selected">All</button>
+          </li>
+          <li>
+            <button>Active</button>
+          </li>
+          <li>
+            <button>Completed</button>
+          </li>
+        </ul>
+        <button class="clear-completed">Clear completed</button>
+      </footer>
+    );
+  }
+}
+
+export default TaskInputForm;
